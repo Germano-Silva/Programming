3a68b60d90b4f92862e1dc0515f94ca06c56a3b1
Germano-Silva <germano.silva@ufn.edu.br>
2022-12-31 12:03:56 -0300

    Update forca01.c

--- Programming in C++/Material adicional/Alura/02_Avançando na linguagem/Aula_01/forca01.c
+++ Programming in C++/Material adicional/Alura/02_Avançando na linguagem/Aula_01/forca01.c
@@ -6,12 +6,13 @@
  */
 char palavrasecreta[20];
 char chutes[26];
-int tentativas = 0;
+int chutesdados = 0;
 
 /**
  * Imprime o título do jogo
  */
-void abertura() {
+void abertura() 
+{
     printf("/****************/\n");
     printf("/ Jogo de Forca */\n");
     printf("/****************/\n\n");
@@ -20,13 +21,14 @@ void abertura() {
 /**
  * Solicita uma carta ao usuário e a armazena nos chutes do array
  */
-void chuta() {
+void chuta() 
+{
     char chute;
     printf("Qual letra? ");
     scanf(" %c", &chute);
 
-    chutes[tentativas] = chute;
-    tentativas++;
+    chutes[chutesdados] = chute;
+    chutesdados++;
 }
 
 /**
@@ -36,9 +38,10 @@ void chuta() {
  *
  * @return o valor da variável encontrado.
  */
-int jachutou(char letra) {
+int jachutou(char letra) 
+{
     int achou = 0;
-    for(int j = 0; j < tentativas; j++) {
+    for(int j = 0; j < chutesdados; j++) {
         if(chutes[j] == letra) {
             achou = 1;
             break;
@@ -51,9 +54,10 @@ int jachutou(char letra) {
 /**
  * Imprime a palavra com as letras adivinhadas e as letras não adivinhadas
  */
-void desenhaforca() {
+void desenhaforca() 
+{
 
-    printf("Você já deu %d chutes\n", tentativas);
+    printf("Você já deu %d chutes\n", chutesdados);
 
     for(int i = 0; i < strlen(palavrasecreta); i++) {
 
@@ -69,20 +73,61 @@ void desenhaforca() {
 }
 
 /**
+ * Verifica se o jogador adivinhou todas as letras da palavra secreta
+ *
+ * @return o valor da variável i.
+ */
+int acertou()
+{
+    for(int i = 0; i < strlen(palavrasecreta); i++) 
+    {
+        if (!jachutou(palavrasecreta[i]))
+        {
+            return 0;
+        }
+        
+    }
+    return 1;
+}
+
+/**
+ * Verifica se o jogador perdeu o jogo
+ *
+ * @return o valor da variável errors.
+ */
+int enforcou()
+{
+    int erros = 0;
+    for (int i = 0; i < chutesdados; i++) 
+    {
+        int existe = 0;
+        for(int j = 0; j <strlen(palavrasecreta); j++)
+        {
+            if (chutes[i] == palavrasecreta[j])
+            {
+                existe = 1;
+                break;
+            }
+            
+        }
+        if(!existe) erros++;
+    }
+    return erros>=5;
+}
+
+/**
  * Adiciona a palavra secreta para o array palavrasecreta.
  */
-void escolhepalavra() {
+void escolhepalavra() 
+{
     sprintf(palavrasecreta, "MELANCIA");
 }
 
 /**
  * A função abertura() imprime o título do jogo e o nome do autor.
  */
-int main() {
-
-    int acertou = 0;
-    int enforcou = 0;
-
+int main() 
+{
     abertura();
     escolhepalavra();
 
@@ -92,9 +137,9 @@ int main() {
         desenhaforca();
         chuta();
 
-    } while (!acertou && !enforcou);
+    } while (!acertou() && !enforcou());
 
-    }
+}
 
     /*
     
