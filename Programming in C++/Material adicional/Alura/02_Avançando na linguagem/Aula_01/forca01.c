06b8e4be98562e1883d5ce35cbb7d8f0205934a0
Germano-Silva <germano.silva@ufn.edu.br>
2022-12-31 10:26:13 -0300

    Update forca01.c
    Atualização: realizado quebra da função main em diversas funções, adicionado comentarios, criado variaveis globais, adicionado ponteiros.

--- Programming in C++/Material adicional/Alura/02_Avançando na linguagem/Aula_01/forca01.c
+++ Programming in C++/Material adicional/Alura/02_Avançando na linguagem/Aula_01/forca01.c
@@ -1,46 +1,104 @@
 #include <stdio.h>
 #include <string.h>
 
-int main() {
+/**
+ * Variaveis globais
+ */
+char palavrasecreta[20];
+char chutes[26];
+int tentativas = 0;
 
-    char palavrasecreta[20];
+/**
+ * Imprime o título do jogo
+ */
+void abertura() {
+    printf("/****************/\n");
+    printf("/ Jogo de Forca */\n");
+    printf("/****************/\n\n");
+}
+
+/**
+ * Solicita uma carta ao usuário e a armazena nos chutes do array
+ */
+void chuta() {
+    char chute;
+    printf("Qual letra? ");
+    scanf(" %c", &chute);
+
+    chutes[tentativas] = chute;
+    tentativas++;
+}
+
+/**
+ * Verifica se a letra já foi adivinhada
+ *
+ * @param letra a letra que o usuário digitou
+ *
+ * @return o valor da variável encontrado.
+ */
+int jachutou(char letra) {
+    int achou = 0;
+    for(int j = 0; j < tentativas; j++) {
+        if(chutes[j] == letra) {
+            achou = 1;
+            break;
+        }
+    }
+
+    return achou;
+}
+
+/**
+ * Imprime a palavra com as letras adivinhadas e as letras não adivinhadas
+ */
+void desenhaforca() {
+
+    printf("Você já deu %d chutes\n", tentativas);
+
+    for(int i = 0; i < strlen(palavrasecreta); i++) {
+
+        if(jachutou(palavrasecreta[i])) {
+            printf("%c ", palavrasecreta[i]);
+        } else {
+            printf("_ ");
+        }
+
+    }
+    printf("\n");
+
+}
+
+/**
+ * Adiciona a palavra secreta para o array palavrasecreta.
+ */
+void escolhepalavra() {
     sprintf(palavrasecreta, "MELANCIA");
+}
+
+/**
+ * A função abertura() imprime o título do jogo e o nome do autor.
+ */
+int main() {
 
     int acertou = 0;
     int enforcou = 0;
 
-    char chutes[26];
-    int tentativas = 0;
+    abertura();
+    escolhepalavra();
 
-    do {
+    do 
+    {
 
-        for(int i = 0; i < strlen(palavrasecreta); i++) {
-            int achou = 0;
-
-            for(int j = 0; j < tentativas; j++) {
-                if(chutes[j] == palavrasecreta[i]) {
-                    achou = 1;
-                    break;
-                }
-            }
-
-            if(achou) {
-                printf("%c ", palavrasecreta[i]);
-            } else {
-                printf("_ ");
-            }
-        }
-        printf("\n");
-
-        char chute;
-        printf("Qual letra? ");
-        scanf(" %c", &chute);
-
-        chutes[tentativas] = chute;
-        tentativas++;
-
+        desenhaforca();
+        chuta();
 
     } while (!acertou && !enforcou);
 
-}
+    }
 
+    /*
+    
+    No link abaixo, você encontra o projeto até o momento atual do curso.
+    https://github.com/alura-cursos/C-II-Avan-ando-na-linguagem/archive/fff18af8aeb62ccc137c438ad77094542ecedb12.zip
+
+    */
