6d44b27bb83ac12db1dd0f14a03bcb672f20e502
Germano-Silva <germano.silva@ufn.edu.br>
2023-01-07 10:09:13 -0300

    Update mapa.h
    style: alteração do codigo para melhor visualizar e ter uma seguencia logica.

--- Programming in C++/Material adicional/Alura/03_Recursos avançados da linguagem/Aula_01/mapa.h
+++ Programming in C++/Material adicional/Alura/03_Recursos avançados da linguagem/Aula_01/mapa.h
@@ -1,29 +1,37 @@
 #define HEROI '@'
-#define FANTASMA 'F'
 #define VAZIO '.'
 #define PAREDE_VERTICAL '|'
 #define PAREDE_HORIZONTAL '-'
+#define FANTASMA 'F'
 
 struct mapa {
-    char** matriz;
-    int linhas;
-    int colunas; 
+	char** matriz;
+	int linhas;
+	int colunas; 
 };
 
 typedef struct mapa MAPA;
 
-struct posicao{
-    int x;
-    int y;
+void alocamapa(MAPA* m);
+void lemapa(MAPA* m);
+void liberamapa(MAPA* m);
+void imprimemapa(MAPA* m);
+
+struct posicao {
+	int x;
+	int y;
 };
 
 typedef struct posicao POSICAO;
 
-void lemapa(MAPA* m);
-void alocamapa(MAPA* m);
-void liberamapa(MAPA* m);
-void imprimemapa(MAPA* m);
 void encontramapa(MAPA* m, POSICAO* p, char c);
-int ehvalida(MAPA *m, int x, int y);
-int ehVazia(MAPA *m, int x, int y);
-void andanomapa(MAPA *m, int xorigem, int yorigem, int xdestino, int ydestino);
+
+int ehvalida(MAPA* m, int x, int y);
+int ehvazia(MAPA* m, int x, int y);
+
+void andanomapa(MAPA* m, int xorigem, int yorigem, 
+	int xdestino, int ydestino);
+
+void copiamapa(MAPA* destino, MAPA* origem);
+
+int podeandar(MAPA* m, int x, int y);
